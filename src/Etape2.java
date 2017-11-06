fa8a790efe976e237ebf4d8266ee4adce4633625
PowaHub <pierre.vonthron@viacesi.fr>
2017-11-06 11:15:22 +0100

    update étape 2 et 3

--- src/Etape2.java
+++ src/Etape2.java
@@ -1,5 +1,16 @@
 import exia.ipc.entities.IStep2Strategy;
+import exia.ipc.entities.MachineX;
+import exia.ipc.entities.MachineY;
 
-public abstract class Etape2 implements IStep2Strategy {
+public class Etape2 implements IStep2Strategy {
 
+    @Override
+    public void onMachineRequest(MachineX machineX, MachineY machineY) throws Exception {
+        
+    }
+
+    @Override
+    public void onMachineExecute(MachineX machineX, MachineY machineY) throws Exception {
+
+    }
 }
--- src/Etape3.java
+++ src/Etape3.java
@@ -1,5 +1,16 @@
 import exia.ipc.entities.IStep3Strategy;
+import exia.ipc.entities.MachineZ;
+import exia.ipc.entities.Product;
 
-abstract class Etape3 implements IStep3Strategy {
+public class Etape3 implements IStep3Strategy {
 
+    @Override
+    public MachineZ chooseMachine(MachineZ machineZ, MachineZ machineZ1) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void onMachineRequest(Product product, MachineZ machineZ, MachineZ machineZ1, MachineZ machineZ2) throws Exception {
+
+    }
 }
