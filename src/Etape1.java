9c035ead16ad532fd3d563dc3bc16636ae87c795
l0ic68 <maxiloic@gmail.com>
2017-11-06 11:20:57 +0100

    Etape 1

--- src/Etape1.java
+++ src/Etape1.java
@@ -5,7 +5,7 @@ public class Etape1 implements IStep1Strategy
     @Override
     public Product onMachineRequest(InputDock dock, MachineX machine) throws Exception {
         //Checker si dock vide
-            
+            dock.getAvailableProductsCount();
         //Checker si machine vide
 
         //Checker si machine already take products
